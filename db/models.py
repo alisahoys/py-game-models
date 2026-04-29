fa222c0e94b2c1a2650c8ea6414cb43ce9764adb
Alisa Hoys <hoys@protonmail.com>
2026-04-29 18:20:22 +0200

    Fixed style

--- db/models.py
+++ db/models.py
@@ -10,7 +10,11 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="skills")
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.CASCADE,
+        related_name="skills"
+    )
     description = models.TextField(blank=True, null=True)
 
 
@@ -23,6 +27,15 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255, default="")
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="players")
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, related_name="players")
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.CASCADE,
+        related_name="players"
+    )
+    guild = models.ForeignKey(
+        Guild,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="players"
+    )
     created_at = models.DateTimeField(default=timezone.now)
